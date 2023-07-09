8c7ede123eeae3e5f95b7113bb04296c949788cc
MustafaNur <mustafa@drupart.co.uk>
2023-07-09 20:29:51 +0300

    c# From ProjeIliskiliVeriTabaniOrnek

---
+++ Proje/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Data Source=DESKTOP-ABE0UME;Initial Catalog=proje;Integrated Security=True
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-ABE0UME;Initial Catalog=proje;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           SqlDataAdapter da = new SqlDataAdapter("Execute Proje",baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource= dt;
+        }
+    }
+}
